affc928763eb1f7283180af9eb43f76af009feb1
Sylvie-B <unsolo@hotmail.fr>
2021-12-05 12:32:53 +0100

    work on next page

--- appTri.js
+++ appTri.js
@@ -1,5 +1,5 @@
 // get <a> for start button
-let recycle = document.querySelector("a");
+let start = document.querySelector("a");
 
 // get waste frame
 let wasteFrame = document.getElementById("waste").querySelector('div');
@@ -7,9 +7,12 @@ let wasteFrame = document.getElementById("waste").querySelector('div');
 // screen height
 let main = document.querySelector("main");
 main.style.height = innerHeight + "px";
+let resultScreen = document.getElementById('result');
+resultScreen.style.height = innerHeight + "px";
+
 
 // get dust frame
-let dustFrame = document.getElementById('dustFrame').getElementsByTagName('div');
+let dustFrame = document.getElementsByClassName('dropzone');
 
 let dustbin = [
     "url('dustImg/yellowDust.png')",
@@ -24,7 +27,7 @@ for(let i = 0 ; i < dustFrame.length ; i++){
 
 let waste = [
     [0, "url('waste/yellow/bottle01.jpg')"],
-    [0, "url('waste/yellow/assouplissant.jpg)"],
+    [0, "url('waste/yellow/assouplissant.jpg')"],
     [1, "url('waste/green/bocal.png')"],
     [1, "url('waste/green/glassBottle.jpg')"],
     [2, "url('waste/blue/cardboard.jpg')"],
@@ -35,14 +38,11 @@ let waste = [
     [3, "url('waste/brown/food02.jpg')"],
 ]
 
-console.log(waste);
-
-recycle.addEventListener("click", function (event){
-    event.preventDefault();
-    recycle.style.display = 'none';
-    // give waste background = random waste
-    let item = Math.floor(Math.random() * waste.length);
-    wasteFrame.style.backgroundImage = waste[item][1];
+// beginning state
+let line;
+let count;
+restart();
+function restart (){
     // shake array
     for(let i = 0 ; i < waste.length ; i++){
         let tempo = waste[i];
@@ -50,5 +50,42 @@ recycle.addEventListener("click", function (event){
         waste[i] = waste[x];
         waste[x] = tempo;
     }
-    console.log(waste);
-})
+
+    // display none start button
+    line = 0;
+    start.addEventListener("click", function (event){
+        event.preventDefault();
+        start.style.display = 'none';
+
+        // display waste background
+        wasteFrame.style.backgroundImage = waste[line][1];
+
+    })
+
+    //point counter
+    count = 0;
+    // listen dustbin
+    for(let i = 0 ; i < dustFrame.length ; i++){
+        dustFrame[i].addEventListener('click', function (){
+
+            // is the right dustbin ?
+            i === waste[line][0] ? count++ : null;
+
+            // next waste
+            line++;
+
+            // end game
+            line < 10 ? wasteFrame.style.backgroundImage = waste[line][1]
+                : console.log(count + " points");
+            // next page
+            // create element for each line
+            let resultDiv = document.createElement('div');
+            let selectDb = document.createElement('div');
+            selectDb.className = "dustbinUsed";
+            resultDiv.appendChild(selectDb);
+
+
+        })
+    }
+}
+
